5da5969df1ad94bf00b0e84db1257ff474471511
Marco19987 <marco.desimone@unicampania.it>
2025-02-07 16:19:22 +0100

    add composable marker_publisher

---
+++ aruco_ros/launch/marker_publisher_general_composable.launch.py
@@ -0,0 +1,105 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
+from launch.utilities import perform_substitutions
+from launch_ros.actions import Node
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+from launch_ros.actions import LoadComposableNodes, Node
+from launch.conditions import IfCondition
+
+
+
+def generate_launch_description():
+
+    marker_size_arg = DeclareLaunchArgument(
+        'marker_size', default_value='0.05',
+        description='Marker size in m.'
+    )
+
+    camera_frame_arg = DeclareLaunchArgument(
+        'camera_frame', default_value='camera_frame',
+        description='Name of the camera frame'
+    )
+
+    reference_frame = DeclareLaunchArgument(
+        'reference_frame', default_value='base',
+        description='Reference frame. '
+        'Leave it empty and the pose will be published wrt the camera frame. '
+    )
+
+    min_marker_size = DeclareLaunchArgument(
+        'min_marker_size', default_value='0.02',
+        description='Minimum size of the marker in m.'
+    )
+
+    detection_mode = DeclareLaunchArgument(
+        'detection_mode', default_value='DM_NORMAL',
+        description='Detection mode. '
+        'DM_NORMAL, DM_VIDEO_FAST, DM_FAST'
+    )
+
+    container_name_to_attach = LaunchConfiguration('container_name_to_attach')
+    container_name_to_attach_arg = DeclareLaunchArgument(
+        'container_name_to_attach', default_value='aruco_container',
+        description='If provided the node will be attached to the container with the given name.'
+    )
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    ld.add_action(marker_size_arg)
+    ld.add_action(camera_frame_arg)
+    ld.add_action(reference_frame)
+    ld.add_action(min_marker_size)
+    ld.add_action(detection_mode)
+    ld.add_action(container_name_to_attach_arg)
+
+
+    aruco_marker_publisher_params = {
+            'image_is_rectified': True,
+            'marker_size': LaunchConfiguration('marker_size'),
+            'reference_frame': LaunchConfiguration('reference_frame'),
+            'camera_frame': LaunchConfiguration('camera_frame'),
+            'min_marker_size': LaunchConfiguration('min_marker_size'),
+            'detection_mode': LaunchConfiguration('detection_mode'),
+        }
+
+    # ld.add_action(ComposableNodeContainer(
+    #     name='aruco_container',
+    #     namespace='robot1',
+    #     package='rclcpp_components',
+    #     executable='component_container',
+    #     composable_node_descriptions=[
+    #         ComposableNode(
+    #             package='aruco_ros',
+    #             plugin='ArucoMarkerPublisher',
+    #             name='aruco_marker_publisher',
+    #             namespace='robot1',
+    #             parameters=[aruco_marker_publisher_params],
+    #             remappings=[('/camera_info', 'color/camera_info'),
+    #                 ('/image', 'color/image_raw')],
+    #             extra_arguments=[{'use_intra_process_comms': True}],
+    #             condition=IfCondition(LaunchConfiguration('container_name_to_attach')),
+    #         ),
+    #     ]
+    # ))
+    
+    ld.add_action(LoadComposableNodes(
+        target_container=LaunchConfiguration('container_name_to_attach'),
+        composable_node_descriptions=[
+            ComposableNode(
+                package='aruco_ros',
+                plugin='ArucoMarkerPublisher',
+                name='aruco_marker_publisher',
+                namespace='',
+                parameters=[aruco_marker_publisher_params],
+                remappings=[('/camera_info', 'color/camera_info'),
+                    ('/image', 'color/image_raw')],
+                extra_arguments=[{'use_intra_process_comms': True}],
+            ),
+        ]
+    ))
+
+
+    return ld
